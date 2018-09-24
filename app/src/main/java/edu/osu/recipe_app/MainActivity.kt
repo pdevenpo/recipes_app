3714d009f30a53f634cadc997d97a985185b7156
Tim <laket13@gmail.com>
2018-09-24 12:49:49 -0400

    Testing Git & Android Studio - seems dumb so far

--- app/src/main/java/edu/osu/recipe_app/MainActivity.kt
+++ app/src/main/java/edu/osu/recipe_app/MainActivity.kt
@@ -1,12 +1,16 @@
 package edu.osu.recipe_app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //I'm adding new things to this project
+        // give me good grade
+
         setContentView(R.layout.activity_main)
     }
 }
