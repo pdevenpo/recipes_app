4651a8d20435147d6eba2ddfb18256792e8dad48
Annette <annette_petty96@yahoo.com>
2018-10-04 18:19:46 -0400

    Added layout and fragments for landscape mode

--- app/src/main/java/edu/osu/recipe_app/MainActivity.java
+++ app/src/main/java/edu/osu/recipe_app/MainActivity.java
@@ -1,5 +1,9 @@
 package edu.osu.recipe_app;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +13,29 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Configuration configInfo = getResources().getConfiguration();
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            FragmentLandscape fragmentLandscape = new FragmentLandscape();
+            fragmentTransaction.replace(android.R.id.content, fragmentLandscape);
+        } else {
+            FragmentPortrait fragmentPortrait = new FragmentPortrait();
+            fragmentTransaction.replace(android.R.id.content, fragmentPortrait);
+        }
+
+        fragmentTransaction.commit();
+        //setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
