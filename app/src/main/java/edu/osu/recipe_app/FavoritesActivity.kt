ee869c7bfbf92053de1f97257776b158564b5788
Tim <laket13@gmail.com>
2018-10-21 16:26:45 -0400

    Basics of Data Persistence with Users via Room

--- app/src/main/java/edu/osu/recipe_app/FavoritesActivity.kt
+++ app/src/main/java/edu/osu/recipe_app/FavoritesActivity.kt
@@ -1,8 +1,9 @@
 package edu.osu.recipe_app
 
-import android.arch.persistence.room.Room
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import edu.osu.recipe_app.ui.Favorites.FavoritesFragment
 
 class FavoritesActivity : AppCompatActivity() {
@@ -10,8 +11,13 @@ class FavoritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val db = Room.databaseBuilder(applicationContext,
-                AppDatabase::class.java, "app-database").allowMainThreadQueries().build()
+        val userRepo = UserRepository(applicationContext)
+
+        val list = userRepo.listUsers()
+
+        for(item in list){
+            Log.d("test", item.name)
+        }
 
         setContentView(R.layout.favorites_activity)
         if (savedInstanceState == null) {
@@ -21,4 +27,10 @@ class FavoritesActivity : AppCompatActivity() {
         }
     }
 
+    fun onClick_Button(v: View) {
+        val userRepo = UserRepository(applicationContext)
+        val user = userRepo.getUser("john")
+
+        userRepo.deleteUser(user)
+    }
 }
--- app/src/main/java/edu/osu/recipe_app/MyAccountActivity.kt
+++ app/src/main/java/edu/osu/recipe_app/MyAccountActivity.kt
@@ -1,27 +1,27 @@
 package edu.osu.recipe_app
 
-import android.arch.persistence.room.Room
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.osu.recipe_app.ui.MyAccount.MyAccountFragment
-
-
 
 class MyAccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val db = Room.databaseBuilder(applicationContext,
-                AppDatabase::class.java, "app-database").allowMainThreadQueries().build()
+        val userRepo = UserRepository(applicationContext)
 
+        val email = "test1@gmail.com"
+        val name = "steve"
+        val password = "password"
 
-        setContentView(R.layout.my_recipes_activity)
+        userRepo.insertUser(email, password, name)
+
+        setContentView(R.layout.my_account_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MyAccountFragment.newInstance())
                     .commitNow()
         }
     }
-
 }
