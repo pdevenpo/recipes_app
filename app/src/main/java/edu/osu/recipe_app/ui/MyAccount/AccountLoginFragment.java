fd8803c16539c0d69a46326c5d88a9d9a3235259
Tim <laket13@gmail.com>
2018-10-24 12:16:47 -0400

    Added ability to list users, delete users from MyAccount

--- app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountLoginFragment.java
+++ app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountLoginFragment.java
@@ -5,26 +5,31 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import edu.osu.recipe_app.MainActivity;
+import java.util.List;
+
 import edu.osu.recipe_app.R;
 import edu.osu.recipe_app.ui.UserData.User;
 import edu.osu.recipe_app.ui.UserData.UserRepository;
 
 public class AccountLoginFragment extends Fragment {
 
+    private final String TAG = "LoginActivityTag";
     private EditText mEmail;
     private EditText mPassword;
     private User mUser;
     private Button mLoginButton;
     private Button mCreateAccountButton;
+    private Button mListUsersButton;
+
     private UserRepository mUserRepository;
+
 
     public AccountLoginFragment() {
     }
@@ -44,7 +49,8 @@ public class AccountLoginFragment extends Fragment {
         mEmail = v.findViewById(R.id.emailLoginEntry);
         mPassword = v.findViewById(R.id.passwordLoginEntry);
         mLoginButton = (Button) v.findViewById(R.id.loginButton);
-        mCreateAccountButton = v.findViewById(R.id.createAccountButton);
+        mCreateAccountButton = (Button) v.findViewById(R.id.createAccountButton);
+        mListUsersButton = (Button) v.findViewById(R.id.listUsersButton);
 
         mEmail.addTextChangedListener(new TextWatcher() {
             @Override
@@ -99,6 +105,18 @@ public class AccountLoginFragment extends Fragment {
 //                } else {
 //                    Toast.makeText(getContext(), "All fields must not be blank", Toast.LENGTH_SHORT).show();
 //                }
+            }
+        });
+
+        mListUsersButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                List<User> listUsers = mUserRepository.listUsers();
+
+                for(int i = 0; i < listUsers.size(); i++){
+                    Log.d(TAG, "User " + i + "- Name: " + listUsers.get(i).getName() + " Email: " + listUsers.get(i).getEmail() + " Password: " + listUsers.get(i).getPassword());
+                }
             }
         });
 
