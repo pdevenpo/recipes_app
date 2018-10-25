017ddec4aff9e47d4aed3e4506ab04ae1a1d5754
Annette <annette_petty96@yahoo.com>
2018-10-24 20:28:52 -0400

    Re add xml files

--- app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountEditActivity.java
+++ app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountEditActivity.java
@@ -1,25 +1,25 @@
-//package edu.osu.recipe_app.ui.MyAccount;
-//
-//import android.os.Bundle;
-//import android.support.v4.app.Fragment;
-//import android.support.v4.app.FragmentManager;
-//import android.support.v7.app.AppCompatActivity;
-//
-//import edu.osu.recipe_app.R;
-//
-//public class AccountEditActivity extends AppCompatActivity {
-//    @Override
-//    public void onCreate(Bundle savedInstanceState){
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.my_account_edit_activity);
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.AccountEditContainer);
-//
-//        if(fragment == null){
-//            fragment = new AccountEditFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.AccountEditContainer, fragment)
-//                    .commit();
-//        }
-//    }
-//}
+package edu.osu.recipe_app.ui.MyAccount;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.osu.recipe_app.R;
+
+public class AccountEditActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_account_edit_activity);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.AccountEditContainer);
+
+        if(fragment == null){
+            fragment = new AccountEditFragment();
+            fm.beginTransaction()
+                    .add(R.id.AccountEditContainer, fragment)
+                    .commit();
+        }
+    }
+}
