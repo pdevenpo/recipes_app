8d80b67d6eacc892c637a8bae2909e7b4cb21ab1
Annette <annette_petty96@yahoo.com>
2018-10-24 20:18:00 -0400

    please work?

--- app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountEditFragment.java
+++ app/src/main/java/edu/osu/recipe_app/ui/MyAccount/AccountEditFragment.java
@@ -1,22 +1,22 @@
-package edu.osu.recipe_app.ui.MyAccount;
-
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-
-public class AccountEditFragment extends Fragment {
-
-    public AccountEditFragment(){
-
-    }
-}
+//package edu.osu.recipe_app.ui.MyAccount;
+//
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.text.Editable;
+//import android.text.TextWatcher;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//
+//public class AccountEditFragment extends Fragment {
+//
+//    public AccountEditFragment(){
+//
+//    }
+//}
