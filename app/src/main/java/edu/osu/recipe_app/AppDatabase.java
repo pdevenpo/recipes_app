a0695eb2725d06034a24b0223d5e02c1825b4b14
Ben Devenport <devenport.3@osu.edu>
2018-10-26 12:37:42 -0400

    recipe db

--- app/src/main/java/edu/osu/recipe_app/AppDatabase.java
+++ app/src/main/java/edu/osu/recipe_app/AppDatabase.java
@@ -3,10 +3,13 @@ package edu.osu.recipe_app;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import edu.osu.recipe_app.ui.MyRecipes.Recipe;
+import edu.osu.recipe_app.ui.MyRecipes.RecipeDao;
 import edu.osu.recipe_app.ui.UserData.User;
 import edu.osu.recipe_app.ui.UserData.UserDao;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Recipe.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+    public abstract RecipeDao recipeDao();
 }
