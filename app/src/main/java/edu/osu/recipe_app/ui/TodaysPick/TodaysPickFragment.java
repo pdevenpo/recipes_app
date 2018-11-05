06a19b73ef89029cfa111bca276370481f99621b
Ben Devenport <devenport.3@osu.edu>
2018-11-05 13:06:43 -0500

    todayspick, favorites recycler view fix

--- app/src/main/java/edu/osu/recipe_app/ui/TodaysPick/TodaysPickFragment.java
+++ app/src/main/java/edu/osu/recipe_app/ui/TodaysPick/TodaysPickFragment.java
@@ -1,25 +1,25 @@
 package edu.osu.recipe_app.ui.TodaysPick;
 
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.List;
 
 import edu.osu.recipe_app.R;
 
 public class TodaysPickFragment extends Fragment {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private RecyclerView mTodaysPickRecyclerView;
+    private RecyclerView.Adapter mTodaysPickAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private String[] mDataset;
-
+    private String[] mTodaysPickList;
+    String[] strings = {"1", "2", "3", "4", "5", "6", "7"};
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -31,22 +31,48 @@ public class TodaysPickFragment extends Fragment {
         super.onCreateView(inflater, parent, savedInstanceState);
         View v = inflater.inflate(R.layout.todays_pick_fragment, parent, false);
 
-        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recipe_list);
 
-        // 2. set layoutManger
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        //populate recyclerview with data
-
-//        // 3. create an adapter
-//         mAdapter = new MyAdapter(mDataset);
-//        // 4. set adapter
-//        recyclerView.setAdapter(mAdapter);
-//        // 5. set item animator to DefaultAnimator
-//        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
+        RecyclerView rv = new RecyclerView(getContext());
+        rv = (RecyclerView) v.findViewById(R.id.recipe_list);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(new TodaysPickAdapter(strings));
         return v;
     }
+
+    public class TodaysPickAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
+        private String[] dataSource;
+        public TodaysPickAdapter(String[] dataArgs){
+            dataSource = dataArgs;
+        }
+
+        @Override
+        public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = new TextView(parent.getContext());
+            SimpleViewHolder viewHolder = new SimpleViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(SimpleViewHolder holder, int position) {
+            holder.textView.setText(dataSource[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.length;
+        }
+    }
+
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+    }
+
+
 
 
 
