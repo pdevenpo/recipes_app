bffc680f1274094fee22e436534b1d66ed9b11bf
Ben Devenport <devenport.3@osu.edu>
2018-11-05 11:51:55 -0500

    favorites recylcer view

--- app/src/main/java/edu/osu/recipe_app/ui/Favorites/FavoritesFragment.java
+++ app/src/main/java/edu/osu/recipe_app/ui/Favorites/FavoritesFragment.java
@@ -1,20 +1,16 @@
-package edu.osu.recipe_app.ui.TodaysPick;
+package edu.osu.recipe_app.ui.Favorites;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import edu.osu.recipe_app.R;
 
-public class TodaysPickFragment extends Fragment {
+public class FavoritesFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -29,15 +25,15 @@ public class TodaysPickFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
         super.onCreateView(inflater, parent, savedInstanceState);
-        View v = inflater.inflate(R.layout.todays_pick_fragment, parent, false);
+        View v = inflater.inflate(R.layout.favorites_fragment, parent, false);
 
-        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recipe_list);
-
-        // 2. set layoutManger
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        //populate recyclerview with data
-
+      RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.favorites_list);
+//
+//        // 2. set layoutManger
+     recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//
+//        //populate recyclerview with data
+//
 //        // 3. create an adapter
 //         mAdapter = new MyAdapter(mDataset);
 //        // 4. set adapter
