f75b5d5a77b911e803a28f78ed19ff913e4b31db
Unknown <annette_petty96@yahoo.com>
2018-11-05 12:23:44 -0500

    WIP on MyRecipes recyclerview

--- app/src/main/java/edu/osu/recipe_app/ui/MyRecipes/MyRecipesActivity.java
+++ app/src/main/java/edu/osu/recipe_app/ui/MyRecipes/MyRecipesActivity.java
@@ -1,38 +1,90 @@
 package edu.osu.recipe_app.ui.MyRecipes;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.UUID;
 
-import org.json.JSONException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-
-import edu.osu.recipe_app.R;
+        import edu.osu.recipe_app.R;
+        import edu.osu.recipe_app.ui.MyRecipes.RecyclerView.ILoadMore;
+        import edu.osu.recipe_app.ui.MyRecipes.RecyclerView.Item;
+        import edu.osu.recipe_app.ui.MyRecipes.RecyclerView.MyAdapter;
 
 public class MyRecipesActivity extends AppCompatActivity {
+
+    List<Item> items = new ArrayList <>();
+    MyAdapter adapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_recipes_fragment_container);
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.MyRecipesFragmentContainer);
+//        setContentView(R.layout.my_recipes_fragment_container);
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.MyRecipesFragmentContainer);
+//
+//        if(fragment == null){
+//            fragment = new MyRecipesFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.MyRecipesFragmentContainer, fragment)
+//                    .commit();
+//        }
 
-        if(fragment == null){
-            fragment = new MyRecipesFragment();
-            fm.beginTransaction()
-                    .add(R.id.MyRecipesFragmentContainer, fragment)
-                    .commit();
+
+        setContentView(R.layout.my_recipes_recyclerview);
+        randomData(10); //generate random placeholder data for the recyclerview
+
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter(recycler, this, items);
+        recycler.setAdapter(adapter);
+
+        //set load more event
+        adapter.setLoadMore(new ILoadMore() {
+            @Override
+            public void onLoadMore() {
+                if(items.size() <= 20){
+                    items.add(null);
+                    adapter.notifyItemInserted(items.size() - 1);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            items.remove(items.size() - 1);
+                            adapter.notifyItemRemoved(items.size());
+
+                            //random more data
+                            int index = items.size();
+                            int end = index + 10;
+                            for(int i = index; i < end; i++){
+                                String name = UUID.randomUUID().toString();
+                                Item item = new Item(name, name.length());
+                                items.add(item);
+                            }
+                            adapter.notifyDataSetChanged();
+                            adapter.setLoaded();
+                        }
+                    }, 5000);
+                } else {
+                    Toast.makeText(MyRecipesActivity.this, "Load data completed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void randomData(int length){
+        //create random data
+        for(int i = 0; i < length; i++){
+            String name = UUID.randomUUID().toString();
+            Item item = new Item(name, name.length());
+            items.add(item);
         }
+
     }
 
 }
---
+++ app/src/main/java/edu/osu/recipe_app/ui/MyRecipes/RecyclerView/Item.java
@@ -0,0 +1,28 @@
+package edu.osu.recipe_app.ui.MyRecipes.RecyclerView;
+
+public class Item {
+
+    private String name;
+    private int length;
+
+    public Item(String name, int length){
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
