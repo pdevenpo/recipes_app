c903f028157587b1e6e85c2ec6cb837ce19936f6
Ben Devenport <devenport.3@osu.edu>
2018-11-20 21:51:13 -0500

    findstore camera movement updates, today's pick style update

--- app/src/main/java/edu/osu/recipe_app/ui/FindStore/FindStoreActivity.java
+++ app/src/main/java/edu/osu/recipe_app/ui/FindStore/FindStoreActivity.java
@@ -1,5 +1,7 @@
 package edu.osu.recipe_app.ui.FindStore;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -7,21 +9,32 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 import android.widget.Toast;
 
 import edu.osu.recipe_app.R;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.widget.Toast.LENGTH_LONG;
 
 public class FindStoreActivity extends AppCompatActivity implements
         GoogleMap.OnMyLocationButtonClickListener,
@@ -38,10 +51,12 @@ public class FindStoreActivity extends AppCompatActivity implements
 
     private GoogleMap mMap;
     private boolean mPermissionDenied = false;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     private double lat = 0.0;
     private double lng = 0.0;
     private LatLng latLng = new LatLng(lat,lng);
+    private LatLng latLng2 = new LatLng(lat,lng);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +66,38 @@ public class FindStoreActivity extends AppCompatActivity implements
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        latLng2 = new LatLng(location.getLatitude(),location.getLongitude());
+                        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng2));
+                        CameraPosition cp = new CameraPosition(latLng2,10.0f,0f,0f);
+                        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp));
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
 
+        if(!mPermissionDenied && lm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    findStore(null);
+                }
+            }, 5000);   //5 seconds
+        }else{
+            Toast.makeText(this, "Please Enable Location Services!\n" + "If it is enabled, please check app permissions." , LENGTH_LONG).show();
+
+        }
     }
 
     @Override
@@ -87,10 +133,11 @@ public class FindStoreActivity extends AppCompatActivity implements
     //On click of button in corner of map, get user's current location and pan camera to location
     @Override
     public void onMyLocationClick(@NonNull Location location) {
-        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Current location:\n" + location, LENGTH_LONG).show();
         lat = location.getLatitude();
         lng = location.getLongitude();
         latLng = new LatLng(lat,lng);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
     }
 
@@ -107,7 +154,10 @@ public class FindStoreActivity extends AppCompatActivity implements
         stringBuilder.append("&keyword="+"store");
         //key is google places api key
         stringBuilder.append("&key="+getResources().getString(R.string.google_places_key));
+        //added 11/20
 
+//        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 5);
+//        mMap.moveCamera(yourLocation);
         String url = stringBuilder.toString();
 
         Object dataTransfer[] = new Object[2];
